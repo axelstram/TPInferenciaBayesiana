e96abaaaf40bb0aeb55ff17f857fee56ba106a0c
axel <astraminsky@dc.uba.ar>
2016-05-17 11:45:57 -0300

    asd

--- tp.m
+++ tp.m
@@ -24,8 +24,9 @@ datastruct = struct('k1',k1,'n1',n1,'k2',k2,'n2',n2,'k3',k3,'n3',n3);
 
 %Initialize Unobserved Variables
 for i=1:nchains
-    S.theta = 0.5; % An Intial Value for the Success Rate
-    S.theta_prime = 0.5; 
+    S.theta1 = 0.5; % An Initial Value for the Success Rate
+    S.theta2 = 0.5;
+    S.theta3 = 0.5;
     init0(i) = S;
 end
 
@@ -42,7 +43,7 @@ fprintf( 'Running JAGS ...\n' );
 	'nburnin', nburnin,...
 	'nsamples', nsamples, ...
 	'thin', nthin, ...
-	'monitorparams', {'theta','theta_prime'}, ...
+	'monitorparams', {'theta1', 'theta2', 'theta3'}, ...
 	'savejagsoutput' , 1 , ...
 	'verbosity' , 1 , ...
 	'cleanup' , 0 , ...
@@ -51,12 +52,28 @@ toc
 
 %Grafico los resultados
 
-figure(3);clf;hold on;
-eps = .01; binsc = eps/2:eps:1-eps/2; binse = 0:eps:1;
-count = histc(reshape(samples.theta,1,[]),binse);
-count = count(1:end-1);
-count = count/sum(count)/eps;
-ph = plot(binsc,count,'k-');
-set(gca,'box','on','fontsize',14,'xtick',[0:.2:1],'ytick',[1:ceil(max(get(gca,'ylim')))]);
-xlabel('Rate','fontsize',16);
-ylabel('Posterior Density','fontsize',16);
+theta1 = samples.theta1();
+theta2 = samples.theta2();
+theta3 = samples.theta3();
+
+histogram(theta1, 'Normalization', 'probability')
+hold on
+%histogram(theta2, 'Normalization', 'pdf')
+hold on
+%histogram(theta3, 'Normalization', 'pdf')
+% mean(theta1)
+% mean(theta2)
+% mean(theta3)
+
+
+
+% 
+% figure(3);clf;hold on;
+% eps = .01; binsc = eps/2:eps:1-eps/2; binse = 0:eps:1;
+% count = histc(reshape(samples.theta,1,[]),binse);
+% count = count(1:end-1);
+% count = count/sum(count)/eps;
+% ph = plot(binsc,count,'k-');
+% set(gca,'box','on','fontsize',14,'xtick',[0:.2:1],'ytick',[1:ceil(max(get(gca,'ylim')))]);
+% xlabel('Rate','fontsize',16);
+% ylabel('Posterior Density','fontsize',16);
